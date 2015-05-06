55208a8c0fd5c93ecf1b50c9fdaebe2872963edb
Sunny K Gupta <sunnylautner4@gmail.com>
2015-05-06 15:36:14 +0530

    Choropleth bug fixed and some file location changed in plottng tweets| need more adjustment in data feeded for Chloropleth

--- Visualization/plot_per_tweets.R
+++ Visualization/plot_per_tweets.R
@@ -13,7 +13,12 @@ library(leaflet)
 plot_per_tweets <- function(file_string){
 
   argument_frame = data.frame( match = c("economy", "religion", "politics", "tourism", "overall"),
-                               files = c("csv/economy_tweets.csv", "csv/religion_tweets.csv", "csv/politic_tweets.csv", "csv/tourism_tweets.csv", "overall_plots")
+
+                               files = c("Visualization/csv/economy_tweets.csv",
+                                         "Visualization/csv/religion_tweets.csv",
+                                         "Visualization/csv/politic_tweets.csv",
+                                         "Visualization/csv/tourism_tweets.csv",
+                                         "overall_plots")
   )
 
   # loop for opening the file
@@ -25,10 +30,11 @@ plot_per_tweets <- function(file_string){
 
   data_frame <- data.frame()
   if(file_string == "overall"){
-    eco <- read.csv(file="csv/economy_tweets.csv",head=TRUE,sep=",")
-    rel <- read.csv(file="csv/religion_tweets.csv",head=TRUE,sep=",")
-    pol <- read.csv(file="csv/politic_tweets.csv",head=TRUE,sep=",")
-    tour <- read.csv(file="csv/tourism_tweets.csv",head=TRUE,sep=",")
+
+    eco <- read.csv(file="Visualization/csv/economy_tweets.csv",head=TRUE,sep=",")
+    rel <- read.csv(file="Visualization/csv/religion_tweets.csv",head=TRUE,sep=",")
+    pol <- read.csv(file="Visualization/csv/politic_tweets.csv",head=TRUE,sep=",")
+    tour <- read.csv(file="Visualization/csv/tourism_tweets.csv",head=TRUE,sep=",")
     data_frame <- rbind(eco, rel, pol, tour)
   }
 
@@ -44,3 +50,11 @@ plot_per_tweets <- function(file_string){
                      radius = 1, color = c('red')) %>% setView(0.0,0.0, zoom =2)
 
 }
+#
+# df <- read.csv(file="Visualization/csv/economy_tweets.csv",head=TRUE,sep=",")
+#
+# mapStates = map("world", fill = TRUE, plot = FALSE)
+# leaflet(mapStates)%>% addTiles()%>%
+#   addPolygons(fillColor = topo.colors(10, alpha = NULL), stroke = FALSE)%>%
+#   addCircleMarkers(lng = df$lng, lat = df$lat,
+#                    radius = 1, color = c('red')) %>% setView(0.0,0.0, zoom =3)
